d9e1df4c4c468ee0914ca30954e837f1474db0c1
Laiba <laibawajahat123@gmail.com>
2022-08-25 22:44:38 +0500

    Added Components

---
+++ src/Components/NavigationBar.vue
@@ -0,0 +1,47 @@
+<template>
+    <header class="header">
+        <nav class="navbar">
+            <div class="container">
+                <div class="navbar-brand">
+                    <a class="navbar-item has-text-white is-size-2 has-text-weight-bold" href="#">
+                        {{ brandName }}
+                    </a>
+                    <span role="button" tabindex="0" class="navbar-burger burger has-text-white"
+                        data-target="navbar-menu">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </span>
+                </div>
+                <div id="navbar-menu" class="navbar-menu">
+                    <div class="navbar-end">
+                        <a 
+                        v-for="item in menuItems"
+                        v-bind:key="item.text"
+                        v-bind:href="item.link"
+                        class="navbar-item nav-web">
+                            {{item.text}}
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            brandName: 'Exchangario',
+            menuItems: [
+                { text: "Home", link: "/" },
+                { text: "About", link: "/about" },
+                { text: "FAQ", link: "/faq" },
+                { text: "Login", link: "/login" },
+                { text: "Register", link: "/register" },
+            ]
+        }
+    }
+}
+</script>
